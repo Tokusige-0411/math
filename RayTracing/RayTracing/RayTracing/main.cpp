e90d0ccde0f741a45848e7fa230c9fe7e6720ab8
Tokushige-0411 <cotamomi0411@gmai.com>
2020-10-27 15:22:31 +0900

    距離求めるまで

--- RayTracing/RayTracing/RayTracing/main.cpp
+++ RayTracing/RayTracing/RayTracing/main.cpp
@@ -1,4 +1,5 @@
 #include<DxLib.h>
+#include<cmath>
 #include"Geometry.h"
 const int screen_width = 640;
 const int screen_height = 480;
@@ -11,7 +12,7 @@ const int screen_height = 480;
 ///@param ray (視点からスクリーンピクセルへのベクトル)
 ///@param sphere 球
 ///@hint レイは正規化しといたほうが使いやすいだろう
-bool IsHitRayAndObject(const Position3& eye,const Vector3& ray,const Sphere& sp) {
+bool IsHitRayAndObject(const Position3& eye,const Vector3& ray,const Sphere& sp ,float& t) {
 	//レイが正規化済みである前提で…
 	//
 	//視点から球体中心へのベクトル(視線)を作ります
@@ -21,28 +22,44 @@ bool IsHitRayAndObject(const Position3& eye,const Vector3& ray,const Sphere& sp)
 	auto dot = Dot(centerVec, ray);
 	//視線ベクトルとベクトル長をかけて、中心からの垂線下した点を求めます
 	//このtmpがR(C・R)となる
-	auto tmp = ray * dot;
-	return ((centerVec - tmp).Magnitude() <= sp.radius);
+	auto V = centerVec - ray * dot;
+	if (V.Magnitude() <= sp.radius)
+	{
+		// 表面の交点tを求める
+		auto w = sqrt((sp.radius * sp.radius) - (V.Magnitude() * V.Magnitude()));
+		t = dot - w;
+		// 交点tへの法線ベクトルを求める
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 ///レイトレーシング
 ///@param eye 視点座標
 ///@param sphere 球オブジェクト(そのうち複数にする)
-void RayTracing(const Position3& eye,const Sphere& sphere) {
+void RayTracing(const Position3& eye,const Sphere& sphere, const Position3& light) {
 
-	Vector3 ray;
 	Position3 screenPos;
+	Vector3 ray;
+	// 光源から球へのベクトル
+	Vector3 lightVec = light - sphere.pos;
+	lightVec.Normalize();
 	for (int y = 0; y < screen_height; ++y) {//スクリーン縦方向
 		for (int x = 0; x < screen_width; ++x) {//スクリーン横方向
 			//①視点とスクリーン座標から視線ベクトルを作る
-			screenPos = { static_cast<float>(x - (screen_width / 2)), static_cast<float>(y - (screen_height / 2)), 0.0f };
+			screenPos = { static_cast<float>(x - (screen_width / 2)), static_cast<float>((screen_height / 2) - y), 0.0f };
 			ray = screenPos - eye;
 			//②正規化しとく
 			ray.Normalize();
+			float t = 0;
 			//③IsHitRay関数がTrueだったら白く塗りつぶす
-			if (IsHitRayAndObject(eye, ray, sphere))
+			if (IsHitRayAndObject(eye, ray, sphere, t))
 			{
-				DrawPixel(x, y, 0xffffff);
+				auto d = (400.0f - t) / 100.0f;
+				DrawPixel(x, y, GetColor(255 * d, 255 * d, 255 * d));
 			}
 			//※塗りつぶしはDrawPixelという関数を使う。
 		}
@@ -57,7 +74,8 @@ int WINAPI WinMain(HINSTANCE , HINSTANCE,LPSTR,int ) {
 
 	auto eye = Vector3(0, 0, 300);
 	auto sphere = Sphere(100, Position3(0, 0, -100));
-	RayTracing(eye, sphere);
+	auto light = Vector3(100, 100, 200);
+	RayTracing(eye, sphere, light);
 
 	WaitKey();
 	DxLib_End();
